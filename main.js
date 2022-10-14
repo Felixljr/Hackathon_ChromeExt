7df05c559e12e3668c9205ab3a93ea39d89fdad1
Felix Leclerc Jr <felixLJR@icloud.com>
2022-10-13 22:26:43 -0400

    boilerplate

---
+++ main.js
@@ -0,0 +1,8 @@
+let baseColor = document.getElementById("main-color").style.color;
+
+document.getElementById("comp-color").color = hsl()
+
+<div id="main-color">main color</div>
+<div id="comp-color">complementary color</div>
+<div id="triad-1">triadic color 1</div>
+<div id="triad-2">triadic color 2</div>
